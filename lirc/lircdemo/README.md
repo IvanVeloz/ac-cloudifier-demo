bca4b767469997322bb45e5aead779f6e02fa611
Ivan Veloz <devel@ivanveloz.com>
2024-05-16 15:58:50 -0400

    Added demo code to transmit an IR command to the air conditioner

---
+++ lirc/lircdemo/README.md
@@ -0,0 +1,7 @@
+# Setup
+Apart from installing LIRC as explained on the `README.md` one level up, building this demo requires:
+```
+sudo apt-get install build-essential liblircclient-dev
+```
+
+The API documentation for `lirc-client` is available [here](https://www.lirc.org/api-docs/html/).
---
+++ lirc/lircdemo/lircdemo.c
@@ -0,0 +1,27 @@
+#ifdef HAVE_CONFIG_H
+# include <config.h>	// this is LIRC code
+#endif
+
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include "lirc_client.h"
+
+int main()
+{
+	printf("LIRC demonstration\n");
+	int r;
+	int fd = lirc_get_local_socket(NULL, 0);
+	if(fd<0) {
+		printf("Failed to open local socket\n");
+		return -1;
+	}
+	if(r = lirc_send_one(fd, "GE-AHP05LZQ2", "KEY_POWER")) goto fail;
+	sleep(3);
+	if(r = lirc_send_one(fd, "GE-AHP05LZQ2", "KEY_POWER")) goto fail;
+	printf("Done.\n");
+	return 0;
+	fail:
+	printf("Failed to send IR command. %s\n",strerror(r));
+	return -1;
+}
