99fec9579007eede8cf0f05619bda4c5737b75e0
Ivan Veloz <devel@ivanveloz.com>
2024-05-22 01:24:21 +0000

    Remove old gstreamer code

--- opencv/opencvdemo/opencvdemo.cpp
+++ opencv/opencvdemo/opencvdemo.cpp
@@ -1,23 +1,10 @@
 #include <opencv2/opencv.hpp>
 #include <unistd.h>
 #include <iostream>
-#include "cv-gstreamer-pipeline.hpp"
 
 int main()
 {
-    //pipeline parameters
-    int capture_width = 640; //1280 ;
-    int capture_height = 480; //720 ;
-    int framerate = 15 ;
-    int display_width = 640; //1280 ;
-    int display_height = 480; //720 ;
 
-    //reset frame average
-    std::string pipeline = gstreamer_pipeline(capture_width, capture_height, framerate,
-                                              display_width, display_height);
-    //std::cout << "Using pipeline: \n\t" << pipeline << "\n\n\n";
-
-	//cv::VideoCapture cap(pipeline, cv::CAP_GSTREAMER);
 	cv::VideoCapture cap(0);
 	if(!cap.isOpened()) {
 		std::cerr << "Unable to open camera!\n";
