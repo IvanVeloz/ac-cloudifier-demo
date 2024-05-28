6b9de233bc088442caacb911ac5eabebcb81967a
Ivan Veloz <devel@ivanveloz.com>
2024-05-28 13:23:49 -0400

    Removed codec change

--- opencv/opencvdemo/opencvdemo.cpp
+++ opencv/opencvdemo/opencvdemo.cpp
@@ -8,11 +8,11 @@
 int main()
 {
 	bool r;
-	int codec = cv::VideoWriter::fourcc('M','J','P','G');
+	//int codec = cv::VideoWriter::fourcc('M','J','P','G');
 	
 	assert( std::system(V4L2_CTL_CMD) == 0);
 	cv::VideoCapture cap(0, cv::CAP_V4L2);
-	assert( cap.set(cv::CAP_PROP_FOURCC, codec) == true);
+	//assert( cap.set(cv::CAP_PROP_FOURCC, codec) == true);
 	assert( cap.set(cv::CAP_PROP_FPS, 15) == true );
 	assert( cap.set(cv::CAP_PROP_EXPOSURE, 400) == true);
 	assert( cap.set(cv::CAP_PROP_FRAME_WIDTH, 2592) == true);
