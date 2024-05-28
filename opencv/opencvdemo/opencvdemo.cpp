ea3c063352756117a82965b6c7a4b11e5e475836
Ivan Veloz <devel@ivanveloz.com>
2024-05-28 13:13:08 -0400

    Linting

--- opencv/opencvdemo/opencvdemo.cpp
+++ opencv/opencvdemo/opencvdemo.cpp
@@ -7,24 +7,31 @@
 
 int main()
 {
+	bool r;
+	int codec = cv::VideoWriter::fourcc('M','J','P','G');
+	
 	assert( std::system(V4L2_CTL_CMD) == 0);
 	cv::VideoCapture cap(0, cv::CAP_V4L2);
+	assert( cap.set(cv::CAP_PROP_FOURCC, codec) == true);
 	assert( cap.set(cv::CAP_PROP_FPS, 15) == true );
 	assert( cap.set(cv::CAP_PROP_EXPOSURE, 400) == true);
 	assert( cap.set(cv::CAP_PROP_FRAME_WIDTH, 2592) == true);
 	assert( cap.set(cv::CAP_PROP_FRAME_HEIGHT, 1944) == true);
+	
 	if(!cap.isOpened()) {
 		std::cerr << "Unable to open camera!\n";
 		return -1;
 	}
+	
 	cv::Mat frame;
-	cap.read(frame);
-	//Mat save_img; cap >> save_img;
-	if(frame.empty()) {
+	r = cap.read(frame);
+	
+	if(r == false) {
 		std::cerr << "Could not get the frame" << std::endl;
 	}
 	else {
 		cv::imwrite("test.jpg", frame);
 	}
+	
 	return 0;
 }
