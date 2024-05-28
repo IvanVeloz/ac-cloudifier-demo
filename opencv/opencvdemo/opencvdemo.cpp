06716783bfbf3f787aa65c0640f1a6aa68b75604
Ivan Veloz <devel@ivanveloz.com>
2024-05-28 10:35:57 -0400

    Added camera configuration and increased resolution

--- opencv/opencvdemo/opencvdemo.cpp
+++ opencv/opencvdemo/opencvdemo.cpp
@@ -1,11 +1,18 @@
 #include <opencv2/opencv.hpp>
 #include <unistd.h>
+#include <cstdlib>
 #include <iostream>
+
+#define V4L2_CTL_CMD "v4l2-ctl --set-ctrl power_line_frequency=2,auto_exposure=1,exposure_time_absolute=400"
 
 int main()
 {
-
-	cv::VideoCapture cap(0);
+	assert( std::system(V4L2_CTL_CMD) == 0);
+	cv::VideoCapture cap(0, cv::CAP_V4L2);
+	assert( cap.set(cv::CAP_PROP_FPS, 15) == true );
+	assert( cap.set(cv::CAP_PROP_EXPOSURE, 400) == true);
+	assert( cap.set(cv::CAP_PROP_FRAME_WIDTH, 2592) == true);
+	assert( cap.set(cv::CAP_PROP_FRAME_HEIGHT, 1944) == true);
 	if(!cap.isOpened()) {
 		std::cerr << "Unable to open camera!\n";
 		return -1;
