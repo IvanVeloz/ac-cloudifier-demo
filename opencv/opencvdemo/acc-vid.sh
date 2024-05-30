12895420d551d8eb86492f0591d2b3adc81b6788
Ivan Veloz <devel@ivanveloz.com>
2024-05-30 13:25:17 -0400

    Switched to UDP streaming and FFMPEG

---
+++ opencv/opencvdemo/acc-vid.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+    MULTICAST_ADDR=127.0.0.1
+else
+    MULTICAST_ADDR=$1
+fi
+
+if [ -z $2 ]; then
+    MULTICAST_PORT=5000	
+else
+    MULTICAST_PORT=$2
+fi
+
+libcamera-vid -t 0 --nopreview --framerate 5 --codec mjpeg --width 1296 --height 972 --inline --listen -o udp://${MULTICAST_ADDR}:${MULTICAST_PORT}
--- opencv/opencvdemo/opencvdemo.cpp
+++ opencv/opencvdemo/opencvdemo.cpp
@@ -1,23 +1,11 @@
 #include <opencv2/opencv.hpp>
 #include <unistd.h>
 #include <iostream>
-#include "cv-gstreamer-pipeline.hpp"
 
 int main()
 {
-    //pipeline parameters
-    int capture_width = 2304;
-    int capture_height = 1296;
-    int framerate = 56 ;
-    int display_width = 2304;
-    int display_height = 1296;
 
-    //reset frame average
-    std::string pipeline = gstreamer_pipeline(capture_width, capture_height, framerate,
-                                              display_width, display_height);
-    std::cout << "Using pipeline: \n\t" << pipeline << "\n\n\n";
-
-	cv::VideoCapture cap(pipeline, cv::CAP_GSTREAMER);
+	cv::VideoCapture cap("udp://127.0.0.1:5000",cv::CAP_FFMPEG);
 	if(!cap.isOpened()) {
 		std::cerr << "Unable to open camera!\n";
 		return -1;
@@ -31,5 +19,6 @@ int main()
 	else {
 		cv::imwrite("test.jpg", frame);
 	}
+	
 	return 0;
 }
