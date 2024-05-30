96281d768c0b1689d998751d6bf9588776a8d03b
Ivan Veloz <devel@ivanveloz.com>
2024-05-30 13:42:51 -0400

    Changed bash for sh

--- opencv/opencvdemo/acc-vid.sh
+++ opencv/opencvdemo/acc-vid.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ -z $1 ]; then
     MULTICAST_ADDR=127.0.0.1
@@ -12,4 +12,4 @@ else
     MULTICAST_PORT=$2
 fi
 
-libcamera-vid -t 0 --nopreview --framerate 5 --codec mjpeg --width 1296 --height 972 --inline --listen -o udp://${MULTICAST_ADDR}:${MULTICAST_PORT}
+libcamera-vid -t 0 --nopreview --framerate 5 --codec mjpeg --width 1296 --height 972 --inline --listen -o udp://$MULTICAST_ADDR:$MULTICAST_PORT
